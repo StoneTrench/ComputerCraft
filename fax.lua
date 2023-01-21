568221cbd025e4cff95571cb0842a349c76beeab
StoneTrench <rlgamer04@gmail.com>
2023-01-21 03:15:47 +0100

    fax update

--- fax.lua
+++ fax.lua
@@ -12,6 +12,21 @@ repeat
     event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
 until channel == FaxPort
 
-printer.newPage()
-printer.write(message)
-printer.endPage()
+-- Paper Width is 25 columns
+
+printer.newPage();
+local columnCounter = 0;
+local rowCounter = 0;
+for i = 1, #message, 1 do
+    local char = message:sub(i, i);
+    columnCounter = columnCounter + 1;
+
+    printer.write(char);
+    printer.setCursor(columnCounter, rowCounter);
+
+    if columnCounter == 25 or char == "\n" then
+        columnCounter = 0;
+        rowCounter = rowCounter + 1;
+    end
+end
+printer.endPage();
