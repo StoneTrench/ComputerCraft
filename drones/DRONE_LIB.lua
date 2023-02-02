b38b0688a68c8918563d4515ea2131be20553e8e
StoneTrench <rlgamer04@gmail.com>
2023-02-02 19:35:36 +0100

    fix

--- drones/DRONE_LIB.lua
+++ drones/DRONE_LIB.lua
@@ -3,17 +3,16 @@ require("LIBS")
 local args = { ... }
 
 if args[1] == "update" then
-    local drone_lib_data = GIT.get("https://raw.githubusercontent.com/StoneTrench/ComputerCraft/master/drones/DRONE_LIB.lua");
-    local libs_data = GIT.get("https://raw.githubusercontent.com/StoneTrench/ComputerCraft/master/libraries/Main/LIBS.lua");
-
     local script = "require(\"LIBS\");";
-    script = script.."UTILITY.fs.writeFile(\"./DRONE_LIB.lua\", \"" .. drone_lib_data .. "\")";
-    script = script.."UTILITY.fs.writeFile(\"./LIBS.lua\", \"" .. libs_data .. "\")";
+    script = script.."local drone_lib_data = GIT.get(\"https://raw.githubusercontent.com/StoneTrench/ComputerCraft/master/drones/DRONE_LIB.lua\");"
+    script = script.."local libs_data = GIT.get(\"https://raw.githubusercontent.com/StoneTrench/ComputerCraft/master/libraries/Main/LIBS.lua\");";
+    script = script.."UTILITY.fs.writeFile(\"./DRONE_LIB.lua\", drone_lib_data);";
+    script = script.."UTILITY.fs.writeFile(\"./LIBS.lua\", libs_data);";
 
     local func, err = load(script, "drone_lib_data", "t", _ENV)
 
     if func == nil then
-        error("Function failed: " .. err)
+        error("Function failed: " .. script)
     end
 
     local success, msg = pcall(func, select(3))
