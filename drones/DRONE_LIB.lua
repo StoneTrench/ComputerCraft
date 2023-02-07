dfe1c9416de8ab494fee2b46908f25792ca158d0
StoneTrench <rlgamer04@gmail.com>
2023-02-07 22:24:56 +0100

    radarscan

--- drones/DRONE_LIB.lua
+++ drones/DRONE_LIB.lua
@@ -4,7 +4,8 @@ local function DRONE_FUNC()
     local mainPeripherals = {
         helm = nil,
         wireless_modem = nil,
-        reader = nil
+        reader = nil,
+        radar = nil
     }
 
     return {
@@ -13,6 +14,7 @@ local function DRONE_FUNC()
             mainPeripherals.wireless_modem = UTILITY.table.find({ peripheral.find("modem") },
                     function(e) return e.isWireless() end);
             mainPeripherals.reader = peripheral.find("reader")
+            mainPeripherals.radar = peripheral.find("radar")
 
             if DRONE.hasHelm() then
                 mainPeripherals.helm.assemble();
@@ -27,11 +29,23 @@ local function DRONE_FUNC()
         hasReader = function()
             return mainPeripherals.reader ~= nil;
         end,
+        hasRadar = function()
+            return mainPeripherals.radar ~= nil;
+        end,
         gotoLine = function(x, y, z)
             if DRONE.hasReader() then
-                mainPeripherals.reader.transformPosition(x, y, z);--mainPeripherals.helm.
+                mainPeripherals.reader.transformPosition(x, y, z); --mainPeripherals.helm.
+                return true
             end
+
+            return false
         end,
+        radarScan = function(range)
+            if DRONE.hasRadar() then
+                return { mainPeripherals.radar.scan(range) }; --mainPeripherals.helm.
+            end
+            return nil
+        end
     }
 end
 
